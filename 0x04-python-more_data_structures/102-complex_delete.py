44ba97fd3db62fa153684bd531630bd0cc596a41
Lexxyla <bamideleadefolaju@gmail.com>
2022-06-08 17:18:21 +0000

    Delete by value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
