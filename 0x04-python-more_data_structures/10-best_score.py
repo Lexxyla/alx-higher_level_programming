d50a5fa61205437bb806431d8aa82e43856622e7
Lexxyla <bamideleadefolaju@gmail.com>
2022-06-08 17:35:13 +0000

    finds all multiples of 2 in a list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
-    if not a_dictionary:
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return max(a_dictionary)
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
