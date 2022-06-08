cacbd810f99a43f4c35b53f210d3bfe5d5c49234
Lexxyla <bamideleadefolaju@gmail.com>
2022-06-08 18:18:33 +0000

    fix error

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return (list(map(lambda x: x*number, my_list)))
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
