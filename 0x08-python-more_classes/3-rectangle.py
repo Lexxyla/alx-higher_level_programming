336f248582756230b4ab141c7167213f0c02ba85
lexxyla <bamideleadefolaju@gmail.com>
2022-06-27 13:42:13 +0100

    returns _str representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Return the printable representation of the Rectangle.
+        """Return the printable representation of the Rectangle. 
 
         Represents the rectangle with the # character.
         """
