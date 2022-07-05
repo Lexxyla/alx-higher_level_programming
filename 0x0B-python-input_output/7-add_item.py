748b88d94b00a8208831f92d03d3ed0f6e6a0bc2
lexxyla <bamideleadefolaju@gmail.com>
2022-07-05 13:35:26 +0100

    add the functions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
