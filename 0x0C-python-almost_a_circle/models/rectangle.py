c56cc2d7463c14ecd918f86dca1ba00c54f9d17f
Lexxyla <bamideleadefolaju@gmail.com>
2022-07-11 14:29:46 +0100

    test

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.new_method(id)
+
+    def new_method(self, id):
+        super().__init__(id)
