b35f6d457ee68335971a7d2d9d69b6c5ba126cf1
lexxyla <bamideleadefolaju@gmail.com>
2022-08-09 16:00:08 +0100

    uses count

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
--- Displays the number of records with id = 89 in the table first_table in my MySQL server..
+-- Displays the number of records with id = 89 in the table first_table in my MySQL server.
 SELECT COUNT(*)
 FROM `first_table`
 WHERE `id` = 89;
