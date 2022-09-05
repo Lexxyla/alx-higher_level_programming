2b9729e91aed2e57d80571ef3c50ce780c29d2af
lexxyla <bamideleadefolaju@gmail.com>
2022-09-05 21:40:05 +0100

    fix style

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-
 const myVar = 'Javascript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
-console.log('C is fun);
-console.log(Python is cool);
+console.log('C is fun');
+console.log('Python is cool');
 console.log('Javascript is amazing');
