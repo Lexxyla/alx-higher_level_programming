21dc0bee22093b048d1dda9225a84fb1e87fcf73
lexxyla <bamideleadefolaju@gmail.com>
2022-09-05 22:03:53 +0100

    fix code block

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
 myObject.incr = function () {
   this.value++;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
