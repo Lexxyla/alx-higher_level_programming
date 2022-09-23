454959b152b7309800988044a52e2d91b90585bb
Lexxyla <bamideleadefolaju@gmail.com>
2022-09-23 05:04:06 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A script that
-- fetches https://intranet.hbtn.io/status.
+- fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
 """
 
@@ -8,7 +8,7 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
